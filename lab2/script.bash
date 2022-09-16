4307c660dc907aff02b4d54a9cecd5a921b941d4
vivichv9 <110079667+vivichv9@users.noreply.github.com>
2022-09-17 00:17:17 +0300

    Update script.bash

--- lab2/script.bash
+++ lab2/script.bash
@@ -1,1 +1,28 @@
-
+#!/bin/bash
+echo "В какой директории вы хотите удалить пустые файлы? Y -  выбрать текущую, N - ввести вручную"
+choice=""
+read choice
+if [ $choice = "Y" ]
+then
+mydir=$(pwd)
+emptyFiles=$(find $mydir -empty)
+        if rm $emptyFiles
+        then
+        echo "Пустые файлы были удалены, скрипт завершен"
+        else
+        echo "Пустые файлы не найдены"
+        fi
+elif [ $choice = "N" ]
+then
+mydir=""
+read mydir
+emptyFiles=$(find $mydir -empty)
+        if rm $emptyFiles
+        then
+        echo "Пустые файлы были удалены, скрипт завершен"
+        else
+        echo "Пустые файлы не найдены"
+        fi
+else
+echo "Вы ввели неправильные данные, перезапустите скрипт"
+fi
