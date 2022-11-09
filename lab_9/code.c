7f44d3a85eec24a4df9bb2b1cabd99468076b9c4
vivichv9 <110079667+vivichv9@users.noreply.github.com>
2022-11-10 01:15:27 +0300

    Bug fix

--- lab_9/code.c
+++ lab_9/code.c
@@ -1,37 +1,25 @@
 #include <stdio.h>
 
-// Variant 24
-
 int sign(int a) {
-    if (a > 0) return 1;
-    if (a == 0) return 0;
-    return -1;
+    if (a < 0) {
+        return -1;
+    } else if (a == 0) {
+        return 0;
+    } else {
+        return 1;
+    }
 }
 
 int min(int a, int b) {
-    if (a < b) return a;
-    return b;
+    return a < b ? a : b;
 }
 
 int max(int a, int b) {
-    if (a < b) return b;
-    return a;
+    return a > b ? a : b;
 }
 
-int abs(int b) {
-    if (b < 0) b *= -1;
-    return b;
-}
-
-int EvkMod(int a, int b) {
-    int r = a - b * (a/b);
-    if (r < 0) r += abs(b);
-    return r;
-}
-
-int EvkDiv(int a, int b) {
-    int r = EvkMod(a, b);
-    return (a - r) / b;
+int abs(int a) {
+    return a < 0 ? a *= -1 : a;
 }
 
 int CheckPoint(int ax, int ay, int bx, int by, int cx, int cy, int px, int py) {
@@ -43,18 +31,22 @@ int CheckPoint(int ax, int ay, int bx, int by, int cx, int cy, int px, int py) {
 }
 
 int main() {
-    const int i0=20, j0=0, l0=11, ax=-10, ay=0, bx=0, by=10, cx=-10, cy=20;
-    int i = i0, j = j0, l = l0, k = 0, i_next, j_next;
-    for (;k <= 50;k++){
-        i_next = EvkMod((i-k)*max(j, l) + (j-k)*min(i, l) + (l-k)*max(i,j), 23);
-        j_next = EvkMod(-((i-k)*min(j, l) + (j-k)*max(i, l) + (l-k)*min(i,j)), 27);
-        l = abs(i+j-l-k)*sign(i-j+l-k);
-        i = i_next;
-        j = j_next;
+    const int i0 = -8, j0 = -5,l0 = 12, ax = 10,ay = 0, bx = 0,by = 10,cx = -10, cy = 20;
+
+    int i = i0, j = j0, l = l0, k = 0, n = 0;
+
+    for (n = 0; n < 50; n++) {
+        i = ((i * i) / (abs(j - l) + k + 1) - (j * j) / (abs(i - l) + k + 1)) % 30;
+        j = sign(l) * min(i, j) - sign(j) * max(i, l) + k;
+        l = ((i - j) * (j - l) * (l - i)) % 20;
         if (CheckPoint(ax, ay, bx, by, cx, cy, i, j)) break;
     }
-    if (k <= 50) printf("The point hit the area in the step %d", k);
-    else printf("The point didn't hit the area");
-    printf("\nEnd time of the movement - %d. Coordinates of the end point - (%d, %d). The value of dynamic parameter - %d\n", k, i, j, l);
+    if (k <= 50) {
+        printf("Точка попала в окрестность за %d шагов", n);
+    } else {
+        printf("Точка не попала в окресность");
+    }
+    printf("\nКонец движения точки - %d. Координаты конечной точки - (%d, %d). Значение динамического параметра - %d\n", n, i, j, l);
+
     return 0;
 }
