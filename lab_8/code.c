af425309d7844dd0983d79d765aa974f389688d0
vivichv9 <110079667+vivichv9@users.noreply.github.com>
2022-11-14 11:52:44 +0300

    Update code.c

--- lab_8/code.c
+++ lab_8/code.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <assert.h>
 
 int min(int x, int y) {
     return x < y ? x : y;
@@ -8,12 +9,22 @@ int max(int x, int y) {
     return x > y ? x : y;
 }
 
+void test_min() {
+    int x = 5;
+    int y = 3;
+    assert(min(x, y) == 3);
+
+    x = 0;
+    y = 20;
+    assert(min(x, y) == 0);
+}
+
+
 int main() {
+    test_min();
 
     int count, digit1, digit2, sum, razn = 1000000;
-
     scanf("%d", &count);
-
     for (int i = 0; i < count; i++) {
         scanf("%d%d", &digit1, &digit2);
         sum += max(digit1, digit2);
@@ -21,10 +32,12 @@ int main() {
             razn = min(max(digit1, digit2) - min(digit1, digit2), razn);
         }
     }
-    if (sum % 3 == 0) {
-        printf("%d %d", sum - razn);
-    } else {
-        printf("%d %d", sum);
+    if (count != 0 && sum % 3 == 0) {
+        printf("%d", sum - razn);
+    } else if (count != 0 && sum % 3 != 0){
+        printf("%d", sum);
+    } else if (count == 0) {
+        printf("%d", 0);
     }
     return 0;
 }
